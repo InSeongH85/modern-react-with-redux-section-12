b4240794fb374288e25f981ccce9082b1b881503
tanbbang01@gmail.com <tanbbang01@gmail.com>
2021-01-25 22:27:48 +0900

    Created DropDownList Used Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+
 const items = [
   {
     title: 'What is React?',
@@ -16,10 +18,30 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red',
+  },
+  {
+    label: 'The Color Green',
+    value: 'green',
+  },
+  {
+    label: 'A shade blue',
+    value: 'blue',
+  },
+];
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
     </div>
   );
 };
